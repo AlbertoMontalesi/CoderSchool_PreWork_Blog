9489d8727a7e059915751dcb8dd83bbd7d2e632a
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-02-08 13:57:32 +0700

    tweaked some errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-   
 end
-=======
-end
->>>>>>> 019e8dd6ab80b5eb17479f2dfaa3eedcecc35c40
